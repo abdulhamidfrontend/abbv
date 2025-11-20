6eef35b1f4a6c681e1b359b71793647fe73fb429
712 <khamidullayew@gmail.com>
2025-11-20 10:36:02 +0500

    "first"

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./index.css";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Home from "./Pages/home";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Pages/home.tsx
@@ -0,0 +1,12 @@
+const Home = () => {
+  return (
+    <div className="bg-black relative flex items-center justify-center w-full h-screen">
+      <h1 className="text-white text-center text-5xl font-semibold">
+        AB
+        <span className="text-red-700">BV</span>
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
